5d3f8cf3751796e5f70bd45c2f61478fb678a3f4
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-06-28 18:32:01 -0400

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -37,9 +37,16 @@ create table invoice_items (
     id serial PRIMARY KEY,
     unit_price double precision,
     quantity integer,
+    total_price double precision,
     invoice_id integer references invoices(id),
     treatments_id integer references treatments(id)
 );
+
+create index idx_invoice_items on invoice_items(invoice_id, treatments_id);
+create index idx_invoices on invoices(medical_histories_id);
+create index idx_medical_histories on medical_histories(patient_id);
+create index idx_histories_tratments on histories_tratments(medical_histories_id, treatments_id);
+
 
 
 
